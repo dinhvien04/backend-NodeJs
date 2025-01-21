4348a315d18e477b0de17847bc7c1d8b3f7a3c50
dinhvien04 <nguyendinhvien2004@gmail.com>
2025-01-21 21:15:44 +0700

    video 23

--- src/sever.js
+++ src/sever.js
@@ -1,25 +1,27 @@
 const express = require('express'); // Sử dụng CommonJS
 const path = require('path');
+require('dotenv').config();
+console.log(">>> check env: ", process.env);
+
 const app = express();
+// const port = 3000;
+// const hostname = 'loaclhost';
 
 app.set('views', path.join(__dirname, 'views/'));
 app.set('view engine', 'ejs');
 
+//config static
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Route trang chủ
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/plain; charset=utf-8');
-    res.send('Chào mừng Đình Viễn');
+    res.send('Chào mừng Đình Viễn & nodemon');
 });
-
-// Route thứ hai
 app.get('/abc', (req, res) => {
     res.set('Content-Type', 'text/plain; charset=utf-8');
     res.send('Anh Yêu Em');
 });
-
-
-
-// Route hiển thị HTML
 app.get('/dinhvien', (req, res) => {
     res.set('Content-Type', 'text/html; charset=utf-8'); // Đảm bảo gửi nội dung HTML
     // res.send('<h1>H1</h1>'); // Nội dung HTML
