b3baf00237d893073825ef222744a074fe300f9b
dinhvien04 <nguyendinhvien2004@gmail.com>
2025-01-23 20:17:23 +0700

    video 35

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,16 +1,6 @@
 const connection = require('../config/database');
 const getHome = (req, res) => {
-    let users = [];
-    connection.execute(
-        'SELECT * FROM users u',
-        function (err, results, fields) {
-            users = results;
-            console.log(results); // results contains rows returned by server
-            console.log(fields); // fields contains extra meta data about results, if available
-            console.log('check usser: ', users);
-            res.send(JSON.stringify(users));
-        }
-    );
+    return res.render('home.ejs');
 };
 const getABC = (req, res) => {
     res.send('Anh yeu em');
