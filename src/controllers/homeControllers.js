67befa671343c9738207f5169dc6c3e02cae9ee8
dinhvien04 <nguyendinhvien2004@gmail.com>
2025-01-23 18:59:17 +0700

    video 32

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,5 +1,16 @@
+const connection = require('../config/database');
 const getHome = (req, res) => {
-    res.send('Chao mung dinh vien');
+    let users = [];
+    connection.execute(
+        'SELECT * FROM users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+            console.log('check usser: ', users);
+            res.send(JSON.stringify(users));
+        }
+    );
 };
 const getABC = (req, res) => {
     res.send('Anh yeu em');
